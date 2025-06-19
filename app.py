1e03d11e17ef7f3c85a3625515861f87e5471f87
bharani-sankar <153503002+bharanisankar10@users.noreply.github.com>
2025-06-19 23:22:07 +0530

    added index.html

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    # For development; use gunicorn or similar in production
+    app.run(host='0.0.0.0', port=5000, debug=True)
